b3c329ffbbb4b5fcd0618be367804c3145dbd0c9
Doa Kendeerlee <doaken@Doas-MacBook-Pro-2.local>
2022-12-29 17:47:05 -0500

    second after promises and fs.read

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 // common core imports
-const fs = require('fs');
+//const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
+
+
+const fileOps = async () => {
+    try{
+        // read with promises
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt' ), 'utf-8'   );
+        console.log(data);
+
+        /// deleting starter file
+        await fsPromises.unlink( path.join(__dirname, 'files', 'starter.txt' )  )
+
+
+        // write with promises
+        await fsPromises.writeFile( path.join(__dirname, 'files', 'promiseWrite.txt' ), data  )
+        await fsPromises.appendFile( path.join(__dirname, 'files', 'promiseWrite.txt' ), "\nnew append"  )
+        await fsPromises.rename( path.join(__dirname, 'files', 'promiseWrite.txt' ), path.join(__dirname, 'files', 'promiseComplete.txt' )  )
+
+
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt' ), 'utf-8'   );
+        console.log(newData);
+
+
+    }
+    catch(err){
+        console.error(err);
+
+    }
+
+
+}
+// NOW CALL
+fileOps();
+
 
 
 
@@ -8,16 +42,46 @@ const path = require('path');
 //fs.readFile('./files/starter.txt',  (err,data) => {
 //fs.readFile('./files/starter.txt', 'utf8', (err,data) => {
 //   change to path.join to eliminate / for cross platform...
-fs.readFile(path.join(__dirname, 'files', 'starter.txt' ), 'utf8', (err,data) => {
+/* fs.readFile(path.join(__dirname, 'files', 'starter.txt' ), 'utf8', (err,data) => {
   
     if ( err ) throw( err );
     //onsole.log(data);  // buffer data
     console.log(data.toString());
 }  
-)
+) */
 
 // which is handled first, asynchronous nature of node.js
-console.log("hellow world");    // executed before readFile answer callback
+//console.log("hellow world");    // executed before readFile answer callback
+
+
+// writing files and appending
+// fs.writeFile( pathnameFile, stringDataToWrite, callback with only (err) )
+/* fs.writeFile( path.join(__dirname, "files", "reply.txt"), 'Nice meet...'   , (err)=>{
+    if ( err ) throw err;
+    console.log(`Write complete...`);
+
+
+    // append files same as write if file not there
+    fs.appendFile( path.join(__dirname, "files", "reply.txt"), '\nappending ...'   , (err)=>{
+        if ( err ) throw err;
+        console.log(`Append complete...`);
+
+            fs.rename( path.join(__dirname, "files", "reply.txt"), path.join(__dirname, "files", "newReply.txt")   , (err)=>{
+                if ( err ) throw err;
+                console.log(`rename complete...`);
+        
+            }
+            )
+
+
+    }
+    ) // end append
+
+}
+)
+ */
+
+
 
 
 
